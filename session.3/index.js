199fcec97a9a03d2e142ca708c017db3286b874b
Kunal453 <visionkunal123@gmail.com>
2023-03-07 22:29:22 +0530

    add session 3

---
+++ session.3/index.js
@@ -0,0 +1,65 @@
+// using function
+
+// Multiplication.
+function Multiply(s,d){
+    return s*d
+}
+var z= Multiply(4,5);
+console.log(typeof z, z)
+
+// addition
+function addition(a,b){
+    return a+b
+}
+let h= addition(59,48)
+console.log(typeof h, h)
+
+// subtraction
+function subtraction(k,n){
+    return k-n
+}
+let o= subtraction(10,8)
+console.log(typeof o, o)
+
+// Division
+function Division(s,m){
+    return s/m
+}
+let e=Division(6,3)
+console.log(typeof e,e)
+
+
+
+
+
+
+
+
+
+
+
+// using arrow functions
+// subtraction
+    
+let sub = (x, y) =>{
+    return x - y;
+} 
+console.log(sub(100, 20)); 
+
+// Division
+let div = (g,h) =>{
+    return g/h
+}
+console.log(div(4,2))
+
+// Addition
+let add = (d,f) =>{
+    return d+f
+}
+console.log(add(90,80))
+
+// Multiplication
+let mul = (p,q) =>{
+    return p*q
+}
+console.log(mul(90,80))
---
+++ session.3/operator.js
@@ -0,0 +1,37 @@
+let pole=0
+let flag=1
+if(pole == 0 && flag == 1){
+     console.log("You are fool")
+}
+else{
+    console.log("You not fool")
+}
+
+let Pole=0
+let Flag=1
+if(Pole == 0 || Flag == 0){
+     console.log("You are fool")
+}
+else{
+    console.log("You not fool")
+}
+
+
+
+let p=0
+let f=1
+if(p == 0 || f== 1){
+     console.log("You are fool")
+}
+else{
+    console.log("You not fool")
+}
+
+let P=0
+let F=1
+if(P == 1 || F == 1){
+     console.log("You are fool")
+}
+else{
+    console.log("You not fool")
+}
